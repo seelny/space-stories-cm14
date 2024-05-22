c2dcd74839fbbfd1fee0d9c03e8dfe389ad407e9
MetalSage <74924875+MetalSage@users.noreply.github.com>
2024-05-22 16:48:13 +0400

    Add xeno actions (#2)
    * punch & fling action
    
    * add xeno sound
    
    * add gut & screech actions
    
    * add stomp & charge actions

---
+++ Content.Shared/_CM14/Xenos/Fling/XenoFlingComponent.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+using Content.Shared.Damage;
+using Content.Shared.FixedPoint;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._CM14.Xenos.Fling;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(SharedXenoFlingSystem))]
+public sealed partial class XenoFlingComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public FixedPoint2 PlasmaCost = 0;
+
+    [DataField]
+    public DamageSpecifier Damage = new();
+
+    [DataField, AutoNetworkedField]
+    public float Range = 40;
+
+    [DataField, AutoNetworkedField]
+    public float ThrowStrength = 10;
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan StunTime = TimeSpan.FromSeconds(2);
+
+    [DataField, AutoNetworkedField]
+    public EntProtoId Effect = "CMEffectPunch";
+
+    [DataField, AutoNetworkedField]
+    public SoundSpecifier Sound = new SoundPathSpecifier("/Audio/_CM14/Xeno/alien_claw_block.ogg");
+
+    [DataField, AutoNetworkedField]
+    public Vector2? Charge;
+}
---
+++ Content.Shared/_CM14/Xenos/Punch/XenoPunchComponent.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+using Content.Shared.Damage;
+using Content.Shared.FixedPoint;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._CM14.Xenos.Punch;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(SharedXenoPunchSystem))]
+public sealed partial class XenoPunchComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public FixedPoint2 PlasmaCost = 0;
+
+    [DataField]
+    public DamageSpecifier Damage = new();
+
+    [DataField, AutoNetworkedField]
+    public float Range = 5;
+
+    [DataField, AutoNetworkedField]
+    public float ThrowStrength = 5;
+    
+    [DataField, AutoNetworkedField]
+    public TimeSpan StunTime = TimeSpan.FromSeconds(0);
+
+    [DataField, AutoNetworkedField]
+    public EntProtoId Effect = "CMEffectPunch";
+
+    [DataField, AutoNetworkedField]
+    public SoundSpecifier Sound = new SoundPathSpecifier("/Audio/_CM14/Xeno/alien_claw_block.ogg");
+
+    [DataField, AutoNetworkedField]
+    public Vector2? Charge;
+}
