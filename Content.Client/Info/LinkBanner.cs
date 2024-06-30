1b84b18acc61d08fb6a9a91a4e37f31c64d5a875
MetalSage <metalsage.official@gmail.com>
2024-06-30 12:37:46 +0400

    link button disable

--- Content.Client/Info/LinkBanner.cs
+++ Content.Client/Info/LinkBanner.cs
@@ -58,16 +58,6 @@ namespace Content.Client.Info
             roadmapButton.OnPressed += _ => UserInterfaceManager.GetUIController<RoadmapUIController>().ToggleRoadmap();
             buttons.AddChild(roadmapButton);
 
-            var linkAccount = UserInterfaceManager.GetUIController<LinkAccountUIController>();
-            var linkAccountButton = new Button
-            {
-                Text = Loc.GetString("rmc-ui-link-discord-account"),
-            };
-            linkAccountButton.OnPressed += _ => linkAccount.ToggleWindow();
-            buttons.AddChild(linkAccountButton);
-
-            AddInfoButton("rmc-ui-patreon", CCVars.InfoLinksPatreon);
-
             void AddInfoButton(string loc, CVarDef<string> cVar)
             {
                 var button = new Button { Text = Loc.GetString(loc) };
